39495a1f39834b58da28a7349fb3c18761274c5b
iqrasuhail <ialmani@bsu.edu>
2020-11-12 05:43:42 -0500

    Create Navbar

---
+++ front-end/src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+    {
+        title: 'About EVE',
+        url: './abouteve',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Get Started',
+        url: './getstarted',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Monthly Blog',
+        url: './monthlyblog',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: './contactus',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign Up',
+        url: './signup',
+        cName: 'nav-links-mobile'
+    }
+
+]
