b07c40951c009419dc6bdd697bdd85df9ddec69e
iqrasuhail <ialmani@bsu.edu>
2020-11-12 18:35:20 -0500

    Edit extra spacing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,13 @@
  import React from 'react';
- import Navbar from './components/Navbar/Navbar'
+ import Navbar from './components/Navbar/Navbar';
+ import Home from './components/Pages/Home/Home';
  import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <Navbar/>
+      <Home/>
     </div>
   );
 }
