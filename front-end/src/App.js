9697325b5e74d11e25ac432bc5239d0f10695e54
iqrasuhail <ialmani@bsu.edu>
2020-11-13 06:42:28 -0500

    Edit files

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,16 @@
- import React from 'react';
- import Navbar from './components/Navbar/Navbar';
- import Home from './components/Pages/Home/Home';
- import './App.css';
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom'
+import Sidebar from './components/Sidebar/Sidebar'
+import Navbar from './components/Navbar/Navbar'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar/>
-      <Home/>
-    </div>
+    <Router>
+    <Home/>
+    
+    </Router>
   );
 }
 
