05a0934f1880d6f75ccb223a57c3604b379bbb6e
austin <afoster@bsu.edu>
2021-01-26 20:23:30 -0500

    Adds getting started stuff

--- src/components/GetStarted/GetStarted.js
+++ src/components/GetStarted/GetStarted.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 class GetStarted extends Component {
   render() {
     return (
       <div>
-        <h2>THE GETTING STARTED STUFF</h2>
-
+        <h1>Welcome to Enterprise Virtual Exchange!</h1>
+        <h2>Do you have a membership with us?</h2>
+          <Link to="/login"> Yes, I am a member </Link>
+          <div></div>
+          <Link to="/articles"> No, I am a guest user</Link>
       </div>
     );
   }
