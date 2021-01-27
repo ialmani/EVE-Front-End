e1ea00fd6bcb902145c2e4a40857d91037d3d5b1
austin <afoster@bsu.edu>
2021-01-26 20:51:06 -0500

    Adds more get started stuff

--- src/components/GetStarted/GetStarted.js
+++ src/components/GetStarted/GetStarted.js
@@ -6,7 +6,16 @@ class GetStarted extends Component {
     return (
       <div>
         <h1>Welcome to Enterprise Virtual Exchange!</h1>
-        <h2>Do you have a membership with us?</h2>
+        <h3>EVE is a virtual marketplace for entrepreneurs and enterprises to exchange information
+            and make small business education and connections to other related resources easily
+            accessible through a virtual learning platform.</h3>
+        <h3>Small and micro business owners, along with supporting partner institutions, are able
+            to engage with the platform as a member with an individual or organization profile.</h3> 
+        <h3>To get started - use the LOGIN tab to sign-up as a Member. Through your membership,
+            business owners, partners, and sponsors are able to search and view profiles of enterprises
+            that offer specific tool kits and resources, the ability to build social capital and advance
+            your business.</h3> 
+        <h1>Do you have a membership with us?</h1>
           <Link to="/login"> Yes, I am a member </Link>
           <div></div>
           <Link to="/articles"> No, I am a guest user</Link>
