3f38ddc221587e51203003cdc558c32b6fe72fac
drollins12 <46565695+drollins12@users.noreply.github.com>
2021-01-28 11:23:00 -0500

    More content on ContactUs, Adds ContactUs Css file.

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -4,9 +4,13 @@ import "./ContactUs.css";
 class ContactUs extends Component {
   render() {
     return (
-      <div>
-        <h1>THE CONTACT US STUFF</h1>
-
+      <div class="Contact">
+        <h1>Hello, Do you want to get in contact with us?</h1>
+        <h2> The best way to reach us is through Ask EVE.</h2>
+        <p>Ask EVE is our way to answer any questions you may have about the EVE site.<br></br> 
+          You will email an admin of the site who will read your question(s) and will get back to you between 24 - 72 business hours
+        </p>
+          
       </div>
     );
   }
