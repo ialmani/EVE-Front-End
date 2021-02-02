b60729affaa5aaae06e5547cc7c17c3dc3225669
drollins12 <46565695+drollins12@users.noreply.github.com>
2021-02-02 14:44:20 -0500

    addStyling to contact

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -4,14 +4,15 @@ import "./ContactUs.css";
 class ContactUs extends Component {
   render() {
     return (
-      <div class="Contact">
-        <h1>Hello, Do you want to get in contact with us?</h1>
-        <h2> The best way to reach us is through Ask EVE.</h2>
-        <p>Ask EVE is our way to answer any questions you may have about the EVE site.<br></br> 
-          You will email an admin of the site who will read your question(s) and will get back to you between 24 - 72 business hours
-        </p>
-          
+      <div class="Contact-body">
+        <div class="Contact-section">
+          <div class="Contact-container">
+            <h1>Hello, Do you want to get in contact with us?</h1>
+            <h2 class="text2"> The best way to reach us is through our site assistance Ask EVE.</h2>
+            <p class="text">Ask EVE is our way to answer any questions you may have about the EVE site.<br></br> You will email an admin of the site who will read your question(s) and they try to will get back to you between 24 - 72 business hours.</p>
+          </div>
       </div>
+    </div>
     );
   }
 }
