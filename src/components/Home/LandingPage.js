86c1e183e7b722c9c6ff95dd675f2510bb4880e7
iqrasuhail <ialmani@bsu.edu>
2021-02-23 00:30:06 -0500

    Added responsive landing page

--- src/components/Home/LandingPage.js
+++ src/components/Home/LandingPage.js
@@ -1,48 +1,38 @@
 import React, { Component } from 'react';
 import { Button } from '../Button';
-import './LandingPage.css';
-import PopUpVideo from './PopUpVideo';
 import {Link} from 'react-router-dom';
+import './LandingPage.css';
+import Logo from '../../assets/eve_logo.svg'
 
-class LandingPage extends Component {
-state = {
-   seen: false
-   };
-  togglePop = () => {
-   this.setState({
-    seen: !this.state.seen
-   });
-  };
 
-render() {
+
+
+const LandingPage = () => {
   return (
     <div className='landing-container'>
       <video className = "landing-video" src='/Videos/video-1.mp4' autoPlay loop muted />
-        
-       <h1 className = "landing-h1">Welcome to EVE</h1>
-        <p className = "landing-p">Bringing Key Resources to Small Businesses and Entrepreneurs</p>
+     
+      <h1 className = "landing-h1">EVE</h1>
+      <p className = "landing-p" >Bringing Key Resources to Entreprenuers</p>
+
       <div className='landingPagebtns'>
+     
         <Button
-            className='btns'
-            buttonStyle='btn--outline'
-            buttonSize='btn--large'>
-          <Link to='get-started'>
-          GET STARTED
-        </Link>
+          className="btns"
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'>GET STARTED <i className="fas fa-arrow-right" />
+        </Button>
+        &nbsp;
+        &nbsp;
+        <Button
+          className="btns"
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'>WATCH TRAILER <i className='far fa-play-circle' />
         </Button>
 
-        <div>
-            <div className="btn" onClick={this.togglePop}>
-              <Button
-                buttonStyle='btn--primary'
-                buttonSize='btn--medium'>WATCH TRAILER <i className='far fa-play-circle' />
-              </Button>
-            </div>
-            {this.state.seen ? <PopUpVideo toggle={this.togglePop} /> : null}
-        </div>
       </div>
     </div>
   );
-}}
+}
 
 export default LandingPage;
