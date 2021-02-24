cf971c57e6d35755e4495555ea7bb3e3a53c7e94
iqrasuhail <ialmani@bsu.edu>
2021-02-24 03:47:06 -0500

    Made resources page responsive

--- src/components/Resources/AllResources.js
+++ src/components/Resources/AllResources.js
@@ -4,46 +4,66 @@ import './AllResources.css'
 import articleImg from '../../assets/articles.jpg'
 import liveImg from '../../assets/liveEvents.jpg'
 import videoImg from '../../assets/video.jpg'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+
+
 
 const AllResources = () => {
       return (
         <div className = "resources-body">
-          <div className = "heading"><h1>Welcome to our collection of resources to help you learn!</h1></div>
-          <div className = "grid">
-
-            <div className = "grid-item">
-              <div className = "card">
-                <img className = "card-img" src = {articleImg}/> 
-                <div className = "card-content">                  
-                  <p className = "card-text"> Now you can access articles with just one click</p>
-                  <button className = "card-btn"><Link to= '/articles'> View Articles</Link></button>
-                </div>
-              </div>
-            </div>
-
-            <div className = "grid-item">
-              <div className = "card">
-                <img className = "card-img" src = {videoImg}></img>            
-                <div className = "card-content">                 
-                  <p className = "card-text"> Tons of categories for you to pick videos from</p>
-                  <button className = "card-btn"><Link to = '/videos'> View Videos</Link></button>
-                </div>
-              </div>
-            </div>
-
-            <div className = "grid-item">
-              <div className = "card">
-                <img className = "card-img" src = {liveImg}></img>
-                <div className = "card-content">                 
-                  <p className = "card-text"> Don't forget to check the live events going on</p>
-                  <button className = "card-btn"><Link> View Events</Link></button>
-                </div>
-              </div>             
-            </div>
-            
+          <div className = "page-container"> 
+          <div className = "heading-r">
+            <h1>Welcome to our Resources Page!</h1>
+          
           </div>
-        </div>
-      );
-  }
+
+          
+        <div className = "card-container">
+          <Card style={{ width: '25rem'}}>
+          <Card.Img src= {articleImg}/>
+          <Card.Body>
+          <Card.Title>Articles</Card.Title>
+          <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">View Articles</Button>
+  </Card.Body>
+    </Card>
+   
+
+         <Card style={{ width: '25rem'}}>
+        <Card.Img src= {videoImg}/>
+        <Card.Body>
+    <Card.Title>Videos</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Watch Videos</Button>
+  </Card.Body>
+    </Card>
+  
+         <Card style={{ width: '25rem'}}>
+        <Card.Img src= {liveImg}/>
+        <Card.Body>
+    <Card.Title>Live Events</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Attend Live</Button>
+  </Card.Body>
+    </Card>
+    </div>
+      </div>
+
+    </div>
+   
+  );
+}
   
   export default AllResources;
+  
