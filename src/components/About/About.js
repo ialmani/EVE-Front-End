9878aa30dcf1406271b33bc3ba18450799d6ed6d
iqrasuhail <ialmani@bsu.edu>
2021-02-25 19:57:52 -0500

    Minor styling changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,14 +12,14 @@ const AboutUs = () => {
             We bring key resources to entrepreneurs enabling them to realize their visions and build social capital.</h3>
             <h3 className = "text2"> EVE provides a driect connection through candid, shared communication.</h3>
             <h3 className= "text2">The mission of EVE is to bring wisdom, key resources, and purposeful content to its users.</h3>
-            <h4 className = "text">Enterprise Virtual Exchange (EVE) is a state of the art, user friendly platform that delivers illuminating 
+            <p className = "text">Enterprise Virtual Exchange (EVE) is a state of the art, user friendly platform that delivers illuminating 
             connections between entrepreneurs, business owners, and specialized resources providers. EVE brings virtual
             training, networking and special events to the entrepreneurs. Many small business owners spend the bulk of
             their time working their business and find it difficult to leave the shop to participate in activities that
             provide information and opportunities for business ideation, growth, and operational management. Therefore,
             we partner with small business advocates, certified Community Development Financial Institutions, and other
             small business resource providers to customize knowledge for business acumen and bring to the end-user an
-            online and easily accessible platform.</h4>
+            online and easily accessible platform.</p>
             </div>
           </div>
         </div>
