413bf7a908572b471ec62e659dcbc93712ccd51b
austin <afoster@bsu.edu>
2021-02-26 10:46:39 -0500

    Ask EVE layout changes

--- src/components/Askeve/Askeve.js
+++ src/components/Askeve/Askeve.js
@@ -8,20 +8,23 @@ class Askeve extends Component {
     render() {
       return (
         <div class='Askeve-body'>
-            <div class='Askeve-container'>
-                <div class='Askeve-heading1'>Ask EVE</div>
-                <div class='Askeve-heading2'>How can Ask EVE help you today?</div>
-                    <div class="text"> First let's start by telling me who you are.
-                        <Input></Input>
-                        Here, you can ask us anything you would like help with.
-                        <Input></Input>
-                        An admin from our site will read your question(s) and will provide a response within 24 - 72 business hours.
-                        <Link to= '/askeve-submission' className= 'Askeve-submission'>
-                        Send my question<span>&rarr;</span>
-                        </Link>
-                    </div>
+          <div class='Askeve-container'>
+            <div class='Askeve-heading1'>Ask EVE</div>
+            <div class='Askeve-heading2'>How can Ask EVE help you today?</div>
+              <div class="text"> 
+                First let's start by telling me who you are.
+                <Input></Input>
+                <div></div>
+                Here, you can ask us anything you would like help with.
+                <textarea></textarea>
+                <div></div>
+                An admin from our site will read your question(s) and will provide a response within 24 - 72 business hours.
+                <Link to= '/askeve-submission' className= 'Askeve-submission'>
+                Send my question<span>&rarr;</span>
+                </Link> 
+              </div>    
             </div>
-        </div>
+          </div>
       );
     }
   }
