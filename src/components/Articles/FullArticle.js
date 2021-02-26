0b1266504faf8a0c1b437a8f9492291d27d83f4c
iqrasuhail <ialmani@bsu.edu>
2021-02-25 23:38:16 -0500

    Added comment section

--- src/components/Articles/FullArticle.js
+++ src/components/Articles/FullArticle.js
@@ -4,6 +4,7 @@ import {useParams} from 'react-router-dom';
  import './FullArticle.css'
 
 import * as Requests from '../../Requests'
+import profilePic from '../../assets/profilepic.png'
 
 
 
@@ -38,9 +39,49 @@ const FullArticle =  ()  => {
                     </div>
                   
                 </div>
+               <div className = "comments-container">
+                   <div className = "row bootstrap snippets bootdeys">
+                       <div className ="col-md-8 col-sm-12">
+                            <div className = "comment-wrapper">
+                                <div className = "panel panel-info">
+                                    <div className = "panel-heading">
+                                        
+                                    </div>
+                                    <div className = "panel-body">
+                                        <textarea className = "form-control" placeholder = "write a comment..." rows = "3"></textarea>
+                                        <br/>
+                                        <button type = "button" className = "btn btn-info pull-right">Post</button>
+                                        <div className = "clearfix"></div>
+                                        <hr/>
+                                        <ul className = "media-list">
+                                            <li className = "media">
+                                                <a href = "#" className = "pull-left">
+                                                    <img src = {profilePic}/>
+                                                </a>
+                                                <div className="media-body">
+                                                {/* <span className="text-muted pull-right">
+                                                <small className="text-muted">30 min ago</small>
+                                                </span> */}
+                                                <strong className="text-success">@MartinoMont</strong>
+                                                    <p>
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                                    Lorem ipsum dolor sit amet.
+                                                    </p>
+                                                </div>
+                                            </li>
+                                           
+                                        </ul>
 
+                                    </div>
+                                </div>
+                            </div>
+                           
+                       </div>
+                   </div>
+               </div>
 
-        </div>
+            </div>
+      
 
     )
 }
