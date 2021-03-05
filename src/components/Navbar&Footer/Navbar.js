3e61ba27fa74a7c1675b6c319c9a94673f43e867
Jamari Brown <jbrown@bsu.edu>
2021-03-04 23:30:43 -0500

    Navbar refactor with Login/Logout functionality

--- src/components/Navbar&Footer/Navbar.js
+++ src/components/Navbar&Footer/Navbar.js
@@ -23,90 +23,39 @@ const logoutUser =(user)=>{
 
     if (localStorage.getItem('token') === 'undefined' || localStorage.getItem('token') === null) {
         menu = (
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mx-auto">
-
-                <li className="nav-item" >
-                    <Link className="nav-link" to="/about">About EVE</Link>
-                </li>
-
-                <li className="nav-item">
-                    <div className="dropdown">
-                        <Link className="nav-link" to="/resources">Resources</Link>
-                        <div className="dropdown-content">
-                            <a href="/articles">Articles</a>
-                            <a href="/videos">Videos</a>
-                            <a href="/events">Events</a>
-                        </div>
-                    </div>
-                </li>
-
-                <li className="nav-item">
-                    <Link className="nav-link" to="/sponsor-packages">Sponsor Packages</Link>
-                </li>
-
-                <li className="nav-item">
-                    <Link className="nav-link" to="/contact-us">Ask EVE</Link>
-                </li>
-
-                <li className="nav-item">
+            <>
+                <li className="nav-btns">
                     <div className="login-signup-btns">
                         <Link className="nav-link" to="/signup">Sign Up</Link>
                     </div>
                 </li>
-                <li className="nav-item">
+
+                <li className="nav-btns">
                     <div className="login-signup-btns">
                         <Link className="nav-link" to="/login">Login</Link>
                     </div>
                 </li>
-            </ul>
-        </div>
+            </>
         )
     } else {
         menu = (
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mx-auto">
-
-                <li className="nav-item" >
-                    <Link className="nav-link" to="/about">About EVE</Link>
-                </li>
-
-                <li className="nav-item">
-                    <div className="dropdown">
-                        <Link className="nav-link" to="/resources">Resources</Link>
-                        <div className="dropdown-content">
-                            <a href="/articles">Articles</a>
-                            <a href="/videos">Videos</a>
-                            <a href="/events">Events</a>
-                        </div>
-                    </div>
-                </li>
-
-                <li className="nav-item">
-                    <Link className="nav-link" to="/sponsor-packages">Sponsor Packages</Link>
-                </li>
-
-                <li className="nav-item">
-                    <Link className="nav-link" to="/contact-us">Ask EVE</Link>
-                </li>
-
+            <>
                 <li className="nav-item">
                     <Link className="nav-link" to="/sponsor-profile">Profile</Link>
                 </li>
 
-                <li className="nav-item">
+                <li className="nav-btns">
                     <div className="login-signup-btns">
                         <Link className="nav-link" to="/login" onClick={logoutUser}>Logout</Link>
                     </div>
                 </li>
-            </ul>
-        </div>
+            </>
         )
     }
 
 
     return (
-        <nav className="navbar sticky-top navbar-expand-lg navbar-dark">
+        <nav className="navbar sticky-top navbar-expand-lg navbar-dark" style={{ background: useLocation().pathname === '/' ? 'transparent' : 'rgb(30,29,91)', marginTop: useLocation().pathname === '/' ? '-113px' : '0px'}}>
             <Link to="/" className="navbar-brand">
                 <img className="nav-logo" src={Logo} alt='logo' />
             </Link>
@@ -115,8 +64,35 @@ const logoutUser =(user)=>{
                 <span className="navbar-toggler-icon"></span>
             </button>
 
-                {menu}
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mx-auto">
 
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/about">About EVE</Link>
+                    </li>
+
+                    <li className="nav-item dropdown">
+                        <div className="dropdown">
+                            <Link className="nav-link" to="/resources">Resources</Link>
+                            <div className="dropdown-content">
+                                <a href="/articles">Articles</a>
+                                <a href="/videos">Videos</a>
+                                <a href="/events">Events</a>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/sponsor-packages">Sponsor Packages</Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/contact-us">Ask EVE</Link>
+                    </li>
+
+                    {menu}
+                </ul>
+            </div>
         </nav>
     );
 };
