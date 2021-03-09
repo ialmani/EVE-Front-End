75213fe04c40df0570e0c51ebd7633bf97159b8f
austin <afoster@bsu.edu>
2021-03-09 17:46:51 -0500

    Styling update for All Resources

--- src/components/Resources/AllResources.js
+++ src/components/Resources/AllResources.js
@@ -1,63 +1,56 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import './AllResources.css'
 import articleImg from '../../assets/articles.jpg'
 import liveImg from '../../assets/liveEvents.jpg'
 import videoImg from '../../assets/video.jpg'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
-
-
-
+import '../../text_config.css'
 
 const AllResources = () => {
-      return (
-        <div className = "resources-body">
-          <div className = "resources">
-          <div className = "resources-img">
-              <img src = {articleImg}/> 
-          </div>
-        <div className = "resources-info">
-          <h1 className = "resources-title"> ARTICLES</h1>
-          <p className = "resources-text">Welcome to our collection of Articles.</p>
-          <Link to = "/articles">
-          <a className = "resources-cta">View Articles</a>
-          </Link>
-        </div>
+  return (
+    <div className = "resources-banner-container">
+      <div className = "resources-banner-text"><text className = "banner">Resources</text></div>
+              
+    <div className = "resources-body">
+              
+        <div className = "resources">
+          <div className = "resources-img"><img src = {articleImg}/> </div>
+            <div className = "resources-info">
+              <text className = "banner">ARTICLES</text>
+                <p className = "resources-text"><text className = "subheader">Browse through our diverse library of helpful Articles.</text></p>
+                  <Link to = "/articles">
+                    <a className = "resources-cta">View Articles</a>
+                  </Link>
+            </div>
         </div>
 
         <div className = "resources">
-          <div className = "resources-img">
-            <img src = {videoImg}/> 
-          </div>
-        <div className = "resources-info">
-          <h1 className = "resources-title"> VIDEOS</h1>
-          <p className = "resources-text">Welcome to our collection of Videos.</p>
-          <Link to = "/videos">
-            <a className = "resources-cta">View Videos</a>
-          </Link>
-        </div>
+          <div className = "resources-img"><img src = {videoImg}/> </div>
+            <div className = "resources-info">
+              <text className = "banner">VIDEOS</text>
+                <p className = "resources-text"><text className = "subheader">Soak up some knowledge with our collection of Videos.</text></p>
+                  <Link to = "/videos">
+                    <a className = "resources-cta">View Videos</a>
+                  </Link>
+            </div>
         </div>
 
-          
+        
         <div className = "resources">
-          <div className = "resources-img">
-              <img src = {liveImg}/> 
-          </div>
-        <div className = "resources-info">
-          <h1 className = "resources-title"> Live Events</h1>
-          <p className = "resources-text">Join our live events!</p>
-          <Link to = "/events">
-          <a  className = "resources-cta">Attend Live</a>
-          </Link>
-        </div>
+          <div className = "resources-img"><img src = {liveImg}/></div>
+            <div className = "resources-info">
+              <text className = "banner">Live Events</text>
+                <p className = "resources-text"><text className = "subheader">Take a look at some Live Events that are happening soon!</text></p>
+                  <Link to = "/events">
+                  <a  className = "resources-cta">Attend Live</a>
+                  </Link>
+            </div>
         </div>
 
+  </div>
 
-      </div>
-   
+  </div>
   );
-}
-  
+  }
+
   export default AllResources;
-  
