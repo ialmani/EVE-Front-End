55840dfb49074aa8f72b987db60032cbc34fe1b1
drollins12 <46565695+drollins12@users.noreply.github.com>
2021-03-28 12:36:47 -0400

    Unification on articles and events page.

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom'
 import profileImg from '../../assets/profilepic.png'
 import articleImg from '../../assets/articles.jpg'
 import './Articles.css'
@@ -16,11 +16,11 @@ const Articles = () => {
         }).catch(() => null);
     },[]);
     return (
-        <div className = "full-page">
-          <div className="search-bar-section">
-            <div className="search-bar-container">
+        <div className = 'full-page'>
+          <div className='search-bar-section'>
+            <div className='search-bar-container'>
               <input className='search-bar'></input>
-              <button className="search-btn"> Search</button>
+              <button className='search-btn'> Search</button>
             </div>
           </div>
           <div className = 'articles-container'>
@@ -28,15 +28,9 @@ const Articles = () => {
               <div className='card mb-4' >
                 <div className='card-body'>
                   <h5 className='card-title'>{article.title}</h5>
-                  <p className='card-text'>
-                    {(article.content).substring(0,100)}...
-                  </p>
-                  <Link to = {'articles/'+article.id}>
-                  <button type='button' class='btn btn-success'>Read More</button>
-                  </Link>
-                  <p className='card-text'>
-                    {article.author}
-                  </p>
+                  <p className='card-text'>{(article.content).substring(0,100)}...</p>
+                  <Link to = {'articles/'+article.id}><button type='button' class='btn btn-success'>Read More</button></Link>
+                  <p className='card-text'>{article.author}</p>
                 </div>
               </div>
             )}
