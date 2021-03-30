e710948b3b12f1de37d85da869ec9f688c17cdef
iqrasuhail <ialmani@bsu.edu>
2021-03-29 20:35:23 -0500

    added search functionality for articles page

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -7,35 +7,78 @@ import SearchBar from './SearchBar'
 
 const Articles = (props) => {
 
-    console.log(props);
-   
   
     const [articles, setArticles] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState([]);
 
-   
+    useEffect(()=>{
+      Requests.getAllArticles().then(data=>{
+          if(data.statusCode !== 404) {
+              setArticles(data)
+          }
+      }).catch(() => null);
+  },[]);
     const searchHandler = (searchTerm) => {
       setSearchTerm(searchTerm);
       if (searchTerm !== ""){
         const newArticleList = articles.filter((article) => {
-          return Object.values(articles).join(" ").toLowerCase().includes(searchTerm.toLowerCase);
+          return Object.values(article).join(" ").toLowerCase().includes(searchTerm.toLowerCase()) ;
+          
         })
         setSearchResults(newArticleList);
-        console.log(newArticleList)
+        console.log(newArticleList);
       }
+      
       else {
         setSearchResults(articles);
       }
     };
 
-    useEffect(()=>{
-        Requests.getAllArticles().then(data=>{
-            if(data.statusCode !== 404) {
-                setArticles(data)
-            }
-        }).catch(() => null);
-    },[]);
+    const renderArticles = (searchTerm) => {
+      if (searchTerm.length <= 0) {
+        return ( 
+        <div className = "articles-container">
+        {articles.map(article=>
+          <div className="card mb-4">
+            <div className="card-body">
+              <h5 className="card-title">{article.title}</h5>
+              <p className="card-text">{(article.content).substring(0,100)}...</p>
+              <Link to = {'articles/'+article.id}>
+                <button class="btn btn-success">Read More</button>
+              </Link>
+              <p className="card-text">
+              <small className="text-muted">{article.author}</small>
+              </p>
+            </div>
+          </div>
+        )}
+      </div>)
+        
+      }
+        return ( 
+          <div className = "articles-container">
+          {searchResults.map(article=>
+            <div className="card mb-4">
+              <div className="card-body">
+                <h5 className="card-title">{article.title}</h5>
+                <p className="card-text">{(article.content).substring(0,100)}...</p>
+                <Link to = {'articles/'+article.id}>
+                  <button class="btn btn-success">Read More</button>
+                </Link>
+                <p className="card-text">
+                <small className="text-muted">{article.author}</small>
+                </p>
+              </div>
+            </div>
+          )}
+        </div>)
+        
+      }
+   
+     
+ 
+    
 
     return (
         <div className = "full-page font-roboto">
@@ -47,23 +90,8 @@ const Articles = (props) => {
 
         />
 
+        {renderArticles(searchTerm)}
 
-          <div className = "articles-container">
-            {articles.map(article=>
-              <div className="card mb-4">
-                <div className="card-body">
-                  <h5 className="card-title">{article.title}</h5>
-                  <p className="card-text">{(article.content).substring(0,100)}...</p>
-                  <Link to = {'articles/'+article.id}>
-                    <button class="btn btn-success">Read More</button>
-                  </Link>
-                  <p className="card-text">
-                  <small className="text-muted">{article.author}</small>
-                  </p>
-                </div>
-              </div>
-            )}
-          </div>
       </div>
 )
 }
