3c33ecaf6ade151c59307bb3ffe20ed032a9764d
afoster1234 <54412433+afoster1234@users.noreply.github.com>
2021-03-31 13:14:01 -0400

    Askeve polishing

--- src/components/Askeve/Askeve.js
+++ src/components/Askeve/Askeve.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react'
-import { Input } from 'semantic-ui-react'
+import React from 'react'
 import emailjs from 'emailjs-com'
 import './Askeve.css'
 
@@ -8,7 +7,7 @@ const Askeve = () => {
   function sendEmail(e) {
     e.preventDefault();
 
-    emailjs.sendForm('gmail', 'ask_eve', e.target, 'user_4nj8nSwZ1kmJEp3h1kqII')
+    emailjs.sendForm('service_afoster', 'ask_eve_autoreply', e.target, 'user_4nj8nSwZ1kmJEp3h1kqII')
       .then((result) => {
         console.log(result.text);
       }, (error) => {
@@ -18,20 +17,27 @@ const Askeve = () => {
   }
 
   return (
-    <div className='askeve-body'>
+    <div className='askeve-body font-roboto'>
       <div className='askeve-container'>
-        <div className='Askeve-heading1'>Ask EVE</div>
-        <div className='Askeve-heading2'>How can Ask EVE help you today?</div>
-        <form onSubmit={sendEmail} className='text'>
-          First let's start by telling me who you are.
-          <Input type='text' name='from_name'></Input>
-          Where should we contant you?
-          <Input type='text' name='email'></Input>
+        <div className='askeve-heading1'>Ask EVE</div>
+        <div className='askeve-heading2'>This simple form provides you with the freedom to ask the EVE team anything you would like!</div>
+        <form onSubmit={sendEmail} type='text'>
+          <div className='askeve-userinfocontatiner'>
+            <div className='askeve-name-container'>
+              <text className='askeve-label'>First, let's start by leting us know who you are.</text>
+              <input type='text' className='askeve-input' name='from_name'></input>
+            </div>
+            <div className='askeve-email-container'>
+              <text className='askeve-label'>Next, lets get your email address.</text>
+              <input type='text' className='askeve-input' name='email'></input>
+            </div> 
+          </div>
+          <div className='askeve-textarea-continer'>
+            <text className='askeve-label'>Here, you can ask us anything you would like help with.</text>
+            <textarea type='text' className='askeve-textarea' name='message'></textarea>
+          </div>
+          <text className='askeve-label'>An admin from our site will read your question(s) and will provide a response within 24 - 72 business hours.</text>
           <br></br>
-          Here, you can ask us anything you would like help with.
-          <textarea name='message'></textarea>
-          <br></br>
-          An admin from our site will read your question(s) and will provide a response within 24 - 72 business hours.
           <button className='askeve-btn'> Send </button>
         </form>
       </div>
