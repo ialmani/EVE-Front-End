cb638ff96259d3e5ff16d4f4fb36bca89fc2ecc3
afoster1234 <54412433+afoster1234@users.noreply.github.com>
2021-04-09 15:11:31 -0400

    Aboutus responsiveness and styling changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,32 +1,31 @@
 import React from 'react'
 import './About.css'
-import Logo from "../../assets/logo_full.PNG"
 
 const AboutUs = () => {
     return (
-      <div className = 'about-body font-roboto'>
-        <div className = "about-section">
-        <img src={Logo} className='about-logo'></img>
-          <div className = "about-container">
-            <text className='about-subheader'>
-              We bring key resources to entrepreneurs enabling them to realize their visions and build social capital.
-              EVE provides our users with a driect connection through candid, shared communication.
-              The mission of EVE is to bring wisdom, key resources, and purposeful content to its users.
-            </text><br></br>
-            <text className='about-paragraph'><br></br>
-              Enterprise Virtual Exchange (EVE) is a state of the art, user friendly platform that delivers illuminating 
-              connections between entrepreneurs, business owners, and specialized resources providers. EVE brings virtual
-              training, networking and special events to the entrepreneurs. Many small business owners spend the bulk of
-              their time working their business and find it difficult to leave the shop to participate in activities that
-              provide information and opportunities for business ideation, growth, and operational management. Therefore,
-              we partner with small business advocates, certified Community Development Financial Institutions, and other
-              small business resource providers to customize knowledge for business acumen and bring to the end-user an
-              online and easily accessible platform.
-            </text>
+      <div className='about-body'>
+        <div className='about-section'>
+          <div className='inner-container'>
+            <h1>About Us</h1>
+            <h3 className='text2'>
+            We bring key resources to entrepreneurs enabling them to realize their visions and build social capital.</h3>
+            <h3 className='text2'> EVE provides a driect connection through candid, shared communication.</h3>
+            <h3 className='text2'>The mission of EVE is to bring wisdom, key resources, and purposeful content to its users.</h3>
+            <p className='text'>Enterprise Virtual Exchange (EVE) is a state of the art, user friendly platform that delivers illuminating 
+            connections between entrepreneurs, business owners, and specialized resources providers. EVE brings virtual
+            training, networking and special events to the entrepreneurs. Many small business owners spend the bulk of
+            their time working their business and find it difficult to leave the shop to participate in activities that
+            provide information and opportunities for business ideation, growth, and operational management.
+            </p>
+            <p className='text'>
+            Therefore, we partner with small business advocates, certified Community Development Financial Institutions, and other
+            small business resource providers to customize knowledge for business acumen and bring to the end-user an
+            online and easily accessible platform.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
 
-export default AboutUs;
+export default AboutUs
