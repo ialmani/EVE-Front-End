92cbce7fe863babc3dd09d38c13d463337680013
iqrasuhail <ialmani@bsu.edu>
2021-04-09 22:55:50 -0500

    fixed styling for articles page

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -43,10 +43,10 @@ const Articles = (props) => {
         {articles.map(article=>
           <div className="card mb-4">
             <div className="card-body">
-              <h5 className="card-title font-roboto">{article.title}</h5>
+              <h5 className="card-title font-roboto article-title">{article.title}</h5>
               <p className="card-text font-roboto">{(article.content).substring(0,100)}...</p>
               <p className='card-text2 font-roboto'>{article.author}</p>
-              <Link to = {'articles/'+article.id}><button type='button' class='btn btn-success font-roboto'>Read More</button></Link>
+              <Link to = {'articles/'+article.id}><button type='button' className='btn btn-success font-roboto article-btn'>Read More</button></Link>
             </div>
           </div>
         )}
