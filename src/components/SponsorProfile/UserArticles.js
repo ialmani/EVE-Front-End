bdd3d3467817a87f093ebd9f33c26f0a58708b4b
Mera Alfawares <maalfawares@bsu.edu>
2021-04-28 08:38:46 -0400

    Fix sponsor profile bug

--- src/components/SponsorProfile/UserArticles.js
+++ src/components/SponsorProfile/UserArticles.js
@@ -6,7 +6,8 @@ import * as Requests from "../../Requests";
 
 const UserArticles = (props) => {
 
-
+    let userID = props.user;
+    let filteredArticles = props.articles.filter(article => article.user_id === userID);
 
     return (
         <div className='profile-articles tab'>
@@ -18,12 +19,21 @@ const UserArticles = (props) => {
                             <tr>
                                 <th className='t-header'>ID</th>
                                 <th className='t-header'>Title</th>
-                                <th className='t-header' >Date</th>
+                                <th className='t-header' >Author</th>
                             </tr>
                         </thead>
                         <tbody>
-                        {props.articles.map(article =>
-                            <Row/>
+                        {filteredArticles.map(article =>
+                            <tr className='table-text'>
+                                <td>{article.id}</td>
+                                <td>{article.title}</td>
+                                <td>{article.author}</td>
+
+                                <td>
+                                    <button data-toggle='modal' data-target='#delete' data-uid='1'
+                                            className='btn-danger btn-sm btnstyle'><span>Delete</span></button>
+                                </td>
+                            </tr>
                         )}
                         </tbody>
                     </table>
